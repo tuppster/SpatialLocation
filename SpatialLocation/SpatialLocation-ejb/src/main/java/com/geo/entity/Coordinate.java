6c88119b2900e84c5b12560927d4921dd5ee6f63
Christopher Tupps <christophertupps@gmail.com>
2016-01-05 14:18:51 -0700

    Entity classes and persistence xml insertion

--- SpatialLocation/SpatialLocation-ejb/src/main/java/com/geo/entity/Coordinate.java
+++ SpatialLocation/SpatialLocation-ejb/src/main/java/com/geo/entity/Coordinate.java
@@ -6,6 +6,7 @@
 package com.geo.entity;
 
 import java.io.Serializable;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -19,6 +20,68 @@ import javax.persistence.Table;
 public class Coordinate implements Serializable {
 
     @Id
-    private int id;
+    @Column(name = "id")
+    private long id;
+    @Column(name = "location_id")
+    private long locationId;
+    @Column(name = "longitude")
+    private double longitude;
+    @Column(name = "latitude")
+    private double latitude;
 
+    /**
+     * @return the id
+     */
+    public long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the locationId
+     */
+    public long getLocationId() {
+        return locationId;
+    }
+
+    /**
+     * @param locationId the locationId to set
+     */
+    public void setLocationId(long locationId) {
+        this.locationId = locationId;
+    }
+
+    /**
+     * @return the longitude
+     */
+    public double getLongitude() {
+        return longitude;
+    }
+
+    /**
+     * @param longitude the longitude to set
+     */
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    /**
+     * @return the latitude
+     */
+    public double getLatitude() {
+        return latitude;
+    }
+
+    /**
+     * @param latitude the latitude to set
+     */
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
 }
